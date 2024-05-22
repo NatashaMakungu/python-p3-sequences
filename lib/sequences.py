6c78920d49ce7043f1b2b5c5a4fad0ef77083b45
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-05-22 14:43:21 +0300

    complete lib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """Prints the Fibonacci sequence up to the given length."""
+    if length <= 0:
+        print([])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    print(sequence[:length])
+
+# Example:
+print_fibonacci(9)
